3701008cbdb62ada657733b0fb9fd5ce0a4e94ad
Otávio Bruzadin <31077442+OtavioBruzadin@users.noreply.github.com>
2025-09-18 23:40:32 -0300

    Adicionando checagem para escala de cinza e algumas refatoracoes

--- main.c
+++ main.c
@@ -6,6 +6,10 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
+
 
 static const char* get_extension(const char* path) {
     const char* dot = strrchr(path, '.');
@@ -26,6 +30,94 @@ static bool is_supported_ext(const char* ext) {
     return false;
 }
 
+SDL_Surface* convertToRGBA(SDL_Surface* surface) {
+    SDL_Surface* converted = SDL_ConvertSurface(surface, SDL_PIXELFORMAT_RGBA32); //Transformarmos em RGBA32 porque é o formato que conhecemos
+    if (!converted) {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,
+                                 "Falha ao converter imagem para RGBA32",
+                                 SDL_GetError(),
+                                 NULL);
+        SDL_Log("Falha ao converter para RGBA32: %s", SDL_GetError());
+        SDL_DestroySurface(surface);
+        return NULL;
+    }
+    return converted;
+}
+
+static bool isGreyScale(SDL_Surface* surface) {
+    if (!surface) return false;
+
+    //bloquear para manipulação
+    if (!SDL_LockSurface(surface)) {
+        SDL_Log("isGreyScale: SDL_LockSurface falhou: %s", SDL_GetError());
+        return false;
+    }
+
+    const int pixeisLargura = surface->w;
+    const int pixeisAltura  = surface->h;
+    Uint32* pixels = (Uint32*)surface->pixels;
+    const int pitchPixels = surface->pitch / 4; // RGBA32
+
+    const SDL_PixelFormatDetails *format = SDL_GetPixelFormatDetails(surface->format);
+    const SDL_Palette *palette = SDL_GetSurfacePalette(surface);
+
+    bool isGrey = true;
+    for (int y = 0; y < pixeisAltura && isGrey; ++y) {
+        for (int x = 0; x < pixeisLargura; ++x) {
+            const int idx = y * pitchPixels + x;
+            Uint8 r, g, b, a;
+            SDL_GetRGBA(pixels[idx], format, palette, &r, &g, &b, &a);
+            if (r != g || g != b) {
+                isGrey = false;
+                break;
+            }
+        }
+    }
+
+    SDL_UnlockSurface(surface);
+    return isGrey;
+}
+
+SDL_Surface* convertToGrey(SDL_Surface* surface) {
+    if (isGreyScale(surface)) {
+        SDL_Log("Imagem já está em escala de cinza; pulando conversão.");
+        return NULL;
+    } else {
+        if (!SDL_LockSurface(surface)) { //Precisa bloquear para podermos manipular
+            SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,
+                                     "Falha ao bloquear superfície",
+                                     SDL_GetError(),
+                                     NULL);
+            SDL_Log("SDL_LockSurface falhou: %s", SDL_GetError());
+            SDL_DestroySurface(surface);
+            return NULL;
+        }
+
+        SDL_Log("Iniciando Conversão para escala de cinza...");
+
+        const int largura_px = surface->w;
+        const int altura_px  = surface->h;
+        Uint32* pixels = surface->pixels;
+        const int pitchPixels = surface->pitch / 4;
+        const SDL_PixelFormatDetails *format = SDL_GetPixelFormatDetails(surface->format);
+        const SDL_Palette *palette = SDL_GetSurfacePalette(surface);
+
+        for (int y = 0; y < altura_px; ++y) {
+            for (int x = 0; x < largura_px; ++x) {
+                const int idx = y * pitchPixels + x;
+                Uint8 r, g, b, a;
+                SDL_GetRGBA(pixels[idx], format, palette, &r, &g, &b, &a);
+                Uint8 gray = (Uint8)(0.2125f * r + 0.7154f * g + 0.0721f * b);
+                pixels[idx] = SDL_MapRGBA(format, palette, gray, gray, gray, a);
+            }
+        }
+
+        SDL_UnlockSurface(surface);
+        SDL_Log("Conversão concluida");
+        return surface;
+    }
+}
+
 void shutdown(void) {
     SDL_Log("Shutdown...");
     SDL_Quit();
@@ -34,20 +126,16 @@ void shutdown(void) {
 int main(int argc, char *argv[]){
     atexit(shutdown);
 
-    const char* WINDOW_TITLE = "Hello, SDL";
-    const int WINDOW_WIDTH = 640;
-    const int WINDOW_HEIGHT = 480;
+    const char* WINDOW_TITLE = "KISHIMOTO EFFECTS";
+    float largura;
+    float altura;
 
     // Init SDL
     if (!SDL_Init(SDL_INIT_VIDEO)){
         SDL_Log("Erro ao iniciar a SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
-
-    // Init Janela
-    SDL_Window *window = NULL;
-    SDL_Renderer *renderer = NULL;
-
+   // checar se o formato é compativel
     if (argc < 2) {
         SDL_Log("Uso: %s <imagem.(png|jpg|jpeg|bmp|gif|tif|tiff|webp)>", argv[0]);
         return SDL_APP_FAILURE;
@@ -69,16 +157,20 @@ int main(int argc, char *argv[]){
         SDL_Log("Erro ao carregar imagem '%s': %s", argv[1], SDL_GetError());
         return SDL_APP_FAILURE;
     }
-    int largura = surface->w;
-    int altura = surface->h;
 
-    window = SDL_CreateWindow(WINDOW_TITLE, largura, altura, 0);
+
+    SDL_Surface* converted = convertToRGBA(surface);
+    surface = convertToGrey(converted);
+
+    largura = surface->w;
+    altura = surface->h;
+
+    SDL_CreateWindowAndRenderer(WINDOW_TITLE, largura, altura, 0,&window, &renderer);
     if (!window) {
         SDL_Log("Erro ao criar a janela: %s", SDL_GetError());
         SDL_DestroySurface(surface);
         return SDL_APP_FAILURE;
     }
-    renderer = SDL_CreateRenderer(window, NULL);
     if (!renderer) {
         SDL_Log("Erro ao criar o renderer: %s", SDL_GetError());
         SDL_DestroyWindow(window);
@@ -87,6 +179,7 @@ int main(int argc, char *argv[]){
     }
 
     SDL_Texture* imagem = SDL_CreateTextureFromSurface(renderer, surface);
+
     SDL_DestroySurface(surface);
 
     if (!imagem) {
@@ -110,7 +203,7 @@ int main(int argc, char *argv[]){
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        SDL_FRect dest = {0.0f, 0.0f, (float)largura, (float)altura};
+        SDL_FRect dest = {0.0f, 0.0f, largura, altura};
         SDL_RenderTexture(renderer, imagem, NULL, &dest);
 
         SDL_RenderPresent(renderer);
